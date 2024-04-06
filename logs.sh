c8c601c230ae7c1b1023f6f5f33b4a9041dfafe9
ToorubatlaUpendra <upendratoorubatla@gmail.com>
2024-04-06 10:46:22 +0530

    functions

---
+++ logs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+NUMBER=$(id -u)
+
+echo "Script name : $0"
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo "ERROR:: THE $2 IS FAILED"
+        exit 1
+    else
+        echo "The $2 is successfull"
+    fi
+}
+if [ $NUMBER -ne 0 ]; 
+then
+    echo "ERROR : Please run script with rot user"
+    exit 1 
+else
+    echo "You are root USER"
+fi
+
+yum install mysql -y 
+
+VALIDATE $? "Installation of Mysql"
+
+yum install git -y 
+
+VALIDATE $? "Git Installation"
