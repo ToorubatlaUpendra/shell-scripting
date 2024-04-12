a896defec217d0cb0f65307197e703e753cd8418
ToorubatlaUpendra <upendratoorubatla@gmail.com>
2024-04-12 20:59:04 +0530

    deleting the old log files

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -2,11 +2,11 @@
 
 DISKUSAGE=$(df -Th | grep -vE 'tmpfs|File' | awk '{print $6}' | cut -d % -f1)
 # echo "$DISKUSAGE"
-THRESHOLD=1
+THRESHOLD=0
 
 while IFS= read line 
 do 
-    usage=$(echo $line  | awk '{print $6}' | cut -d % -f1 )
+    usage=$(echo $line  | awk '{print $6}' | cut -d % -f1)
     Partion=$(echo $line  | awk '{print $6}')
     if [ $usage -ge $THRESHOLD ]
     then
