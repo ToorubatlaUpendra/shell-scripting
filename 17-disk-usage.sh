33cbd8e6aa4fe0f0c6adcbca9bcde635c6b1a0a6
ToorubatlaUpendra <upendratoorubatla@gmail.com>
2024-04-12 21:00:22 +0530

    deleting the old log files

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -7,7 +7,9 @@ THRESHOLD=0
 while IFS= read line 
 do 
     usage=$(echo $line  | awk '{print $6}' | cut -d % -f1)
+    echo "$usage"
     Partion=$(echo $line  | awk '{print $6}')
+    echo "$Partion"
     if [ $usage -ge $THRESHOLD ]
     then
         message+="High usage on the disk $Partion:$usage"
