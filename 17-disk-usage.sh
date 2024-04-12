be9637002768577c3be59bd038262481dc49b9e0
ToorubatlaUpendra <upendratoorubatla@gmail.com>
2024-04-12 20:55:48 +0530

    deleting the old log files

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -1,11 +1,17 @@
 #!/bin/bash 
 
 DISKUSAGE=$(df -Th | grep -vE 'tmpfs|File' | awk '{print $6}' | cut -d % -f1)
-echo "$DISKUSAGE"
-# THRESHOLD=1
+# echo "$DISKUSAGE"
+THRESHOLD=1
 
-# while IFS= read line 
-# do 
-#     usage=$(echo $line  | awk '{print $6}' | cut -d % -f1 )
-#     echo "$usage"
-# done <<< $DISKUSAGE
+while IFS= read line 
+do 
+    usage=$(echo $line  | awk '{print $6}' | cut -d % -f1 )
+    Partion=$(echo $line  | awk '{print $6}')
+    if [ $usage -ge $THRESHOLD ]
+    then
+        message+="High usage on the disk $Partion:$usage"
+    else
+        echo "usage is below threshold"
+    fi
+done <<< $DISKUSAGE
