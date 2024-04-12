119ca290b8662360637c9ab92a8cf70956d84c1b
ToorubatlaUpendra <upendratoorubatla@gmail.com>
2024-04-12 18:58:10 +0530

    deleting the old log files

--- 15-delete-old-logs.sh
+++ 15-delete-old-logs.sh
@@ -3,14 +3,15 @@
 R="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-SOURCE_DIR="/tmp/shell-scripting/"
-if [ ! -d "$SOURCE_DIR" ]; then
-  echo -e"$R$SOURCE_DIR does exist.$N"
+SOURCE_DIR="/tmp/shell-scripting"
+if [ ! -d "$SOURCE_DIR" ] 
+then
+  echo -e "$R$SOURCE_DIR does exist.$N"
 fi
 FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 
 while IFS= read -r line
 do
    echo "DELETING FILE LINE BY LINE:$line"
-
+   rm -rf $line
 done <<< $FILES_TO_DELETE
