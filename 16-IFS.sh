b29ad500a0f1e46cba10e4890e8763338b36228e
ToorubatlaUpendra <upendratoorubatla@gmail.com>
2024-04-12 19:44:24 +0530

    deleting the old log files

--- 16-IFS.sh
+++ 16-IFS.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-R="\e[m32"
-Y="\e[m33"
-N="\e[m0"
+R="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 FILE="/etc/passwd"
 if [ ! -d "$FILE" ] 
 then
@@ -12,5 +12,5 @@ fi
 while IFS=":" read -r USER_NAME PSSWORD USER_ID GROUP_ID USER_ID_INFO HOME_DIRECTORY COMMAND_SHELL
 do 
     echo "$USER_NAME $PSSWORD "
-done < "$FILE"
+done < $FILE
 
