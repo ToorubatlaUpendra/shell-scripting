e2681a79eb0bf55c7013e0940930f0b903ab4b2e
ToorubatlaUpendra <upendratoorubatla@gmail.com>
2024-04-13 11:47:45 +0530

    after adding

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -2,15 +2,14 @@
 
 DISKUSAGE=$(df -Th | grep -vE 'tmpfs|File')
 # echo "$DISKUSAGE"
-THRESHOLD=0
-
+THRESHOLD=1
 while IFS= read line 
 do 
     usage=$(echo $line  | awk '{print $6}' | cut -d % -f1)
-    Partion=$(echo $line  | awk '{print $6}')
+    Partion=$(echo $line  | awk '{print $1}')
     if [ $usage -ge $THRESHOLD ]
     then
-        message+="High usage on the disk $Partion:$usage\n"
+        message+="High usage on the disk $Partion:$usage\n" #for over writing
     else
         echo "usage is below threshold"
     fi
