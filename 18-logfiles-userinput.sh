9447c6e0ecc107d3aa70c73563b3af4f0260decb
ToorubatlaUpendra <upendratoorubatla@gmail.com>
2024-04-13 20:16:20 +0530

    for testing

--- 18-logfiles-userinput.sh
+++ 18-logfiles-userinput.sh
@@ -3,26 +3,27 @@
 # Initialize variables for optional arguments
 optional_arg1=""
 optional_arg2=""
-optional_arg3=""
 
 # Function to display usage
 usage() {
-    echo "Usage: $0 -s <arg1> -a <arg2> [-d <arg3>] [-t <arg4>] [-m <arg5>]"
+    echo "Usage: $0 -a <arg1> -b <arg2> [-c <arg3>] [-d <arg4>] [-e <arg5>]"
     exit 1
 }
 
 # Use getopts to parse options
-while getopts ":s:a:d:t:m:" opt; do
+while getopts ":a:b:c:d:e:" opt; do
     case ${opt} in
-        s ) mandatory_arg1="$OPTARG"
+        a ) mandatory_arg1="$OPTARG"
             ;;
-        a ) mandatory_arg2="$OPTARG"
+        b ) mandatory_arg2="$OPTARG"
+            ;;
+        c )
+            third_arg="$OPTARG"
+            third_arg_required=true
             ;;
         d ) optional_arg1="$OPTARG"
             ;;
-        t ) optional_arg2="$OPTARG"
-            ;;
-        m ) optional_arg3="$OPTARG"
+        e ) optional_arg2="$OPTARG"
             ;;
         \? ) echo "Invalid option: $OPTARG" 1>&2
              usage
@@ -34,22 +35,27 @@ while getopts ":s:a:d:t:m:" opt; do
 done
 shift $((OPTIND -1))
 
+# Check if the third argument is required based on user choice
+if [[ -n "$third_arg_required" ]]; then
+    if [[ -z "$third_arg" ]]; then
+        echo "Third argument is mandatory for archiving."
+        exit 1
+    fi
+else
+    if [[ -z "$third_arg" ]]; then
+        read -p "Enter the third argument (optional for deletion): " third_arg
+    fi
+fi
+
 # Check for mandatory arguments
 if [[ -z "$mandatory_arg1" || -z "$mandatory_arg2" ]]; then
-    echo "Mandatory arguments -s and -a are required."
+    echo "Mandatory arguments -a and -b are required."
     usage
-    if [ $mandatory_arg2="archieve" ]
-    then
-        if [ -z "$optional_arg1" ]
-        then
-            echo "Mandatory argument -d is required."
-            usage
-        fi
 fi
 
 # Output the values
 echo "Mandatory Argument 1: $mandatory_arg1"
 echo "Mandatory Argument 2: $mandatory_arg2"
+echo "Third Argument: $third_arg"
 echo "Optional Argument 1: $optional_arg1"
 echo "Optional Argument 2: $optional_arg2"
-echo "Optional Argument 3: $optional_arg3"
