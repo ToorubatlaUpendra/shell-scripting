a9ed5520fa380f57c50f79d69dc93c78b0b71c0e
ToorubatlaUpendra <upendratoorubatla@gmail.com>
2024-04-13 12:11:16 +0530

    after adding

--- test-script.sh
+++ test-script.sh
@@ -1,17 +1,36 @@
-#!/bin/bash
+# #!/bin/bash
 
-R="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-FILE="/etc/passwd"
-# echo "$FILE"
-if [ ! -d "$FILE" ] 
-then
-  echo -e "$R$SOURCE_DIR does exist.$N"
-fi
+# R="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+# FILE="/etc/passwd"
+# # echo "$FILE"
+# if [ ! -d "$FILE" ] 
+# then
+#   echo -e "$R$SOURCE_DIR does exist.$N"
+# fi
 
-while IFS=":" read -r USER_NAME PSSWORD USER_ID GROUP_ID USER_ID_INFO HOME_DIRECTORY COMMAND_SHELL
+# while IFS=":" read -r USER_NAME PSSWORD USER_ID GROUP_ID USER_ID_INFO HOME_DIRECTORY COMMAND_SHELL
+# do 
+#     echo "$USER_NAME $PSSWORD "
+
+# done < $FILE
+
+#!/bin/bash 
+
+DISKUSAGE=$(df -Th | grep -vE 'tmpfs|File')
+# echo "$DISKUSAGE"
+THRESHOLD=30
+while IFS= read line 
 do 
-    echo "$USER_NAME $PSSWORD "
+    usage=$(echo $line  | awk '{print $6}' | cut -d % -f1)
+    Partion=$(echo $line  | awk '{print $1}')
+    if [ $usage -ge $THRESHOLD ]
+    then
+        message+="High usage on the disk $Partion:$usage\n" #for over writing
+    else
+        echo "usage is below threshold"
+    fi
+done < $DISKUSAGE
 
-done < $FILE
+echo -e "$message"
