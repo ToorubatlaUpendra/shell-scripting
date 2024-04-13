b3cae23c6740f53bd85d67e893269e9f07b7b001
ToorubatlaUpendra <upendratoorubatla@gmail.com>
2024-04-13 16:20:55 +0530

    after adding

--- mail.sh
+++ mail.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 To_Team=$1
-echo "$To_Team"
 Alert_Type=$2
 BODY=$3
 TO_ADDRESS=$4
 SUBJECT=$5
-FINAL=$(sed -e 's/TO_TEAM/$To_Team/g' -e 's/Alert_Type/$Alert_type/g' -e 's/BODY/$BODY/g' template.html) #for -e for temporary change 
+FINAL=$(sed -e "s/TO_TEAM/$To_Team/g" -e "s/Alert_Type/$Alert_Type/g" -e "s/BODY/$BODY/g" template.html) #for -e for temporary change 
 echo "$FINAL" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
