d28d943447a7a0199950d65775496f5c3fa9e543
ToorubatlaUpendra <upendratoorubatla@gmail.com>
2024-04-13 12:04:25 +0530

    after adding

---
+++ test-script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+R="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+FILE="/etc/passwd"
+echo "$FILE"
+if [ ! -d "$FILE" ] 
+then
+  echo -e "$R$SOURCE_DIR does exist.$N"
+fi
+
+while IFS=":" read -r USER_NAME PSSWORD USER_ID GROUP_ID USER_ID_INFO HOME_DIRECTORY COMMAND_SHELL
+do 
+    echo "$USER_NAME $PSSWORD "
+done < $FILE
