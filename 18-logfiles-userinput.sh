c3eec65c9b44c35e69b4165197c23664e9ad7e1a
ToorubatlaUpendra <upendratoorubatla@gmail.com>
2024-04-14 11:18:56 +0530

    testing

--- 18-logfiles-userinput.sh
+++ 18-logfiles-userinput.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-while getopts ":a:b:" opt; do
+while getopts ":s:a:d:t:m" opt; do
   case ${opt} in
-    a )
+    s )
       arg1=$OPTARG
       ;;
-    b )
+    a )
       arg2=$OPTARG
       ;;
+    d )
+      arg3=$OPTARG
+      ;;
+    t )
+      arg2=$OPTARG
+      ;;
+    m )
+      arg2=$OPTARG
+      ;;
+
     \? )
-      echo "Invalid option: $OPTARG"
+      echo "Invalid option: $OPTARG"  #this will though error if we give any wrong option
       exit 1
       ;;
     : )
-      echo "Option -$OPTARG requires an argument."
+      echo "Option -$OPTARG requires an argument." #it will tell this requires an argument 
       exit 1
       ;;
   esac
